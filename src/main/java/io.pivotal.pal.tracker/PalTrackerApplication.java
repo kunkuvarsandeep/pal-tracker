07bc6d2aeab7e03d8daf486b77d0524e5b45f577
kunkuvarsandeep <sandeep.kumar6@dell.com>
2020-05-12 12:13:38 +0100

    Mysql database correction

--- src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
+++ src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
@@ -1,9 +1,10 @@
 package io.pivotal.pal.tracker;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication{
@@ -18,9 +19,9 @@ public class PalTrackerApplication{
     } */
 
     @Bean
-    public TimeEntryRepository timeEntryRepository(){
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+    public TimeEntryRepository timeEntryRepository(DataSource dataSource){
+       /* MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));*/
         return new JdbcTimeEntryRepository(dataSource);
     }
 
